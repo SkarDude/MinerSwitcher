d302132389a3af75d9a881e596766df9316e2578
Alex Hough <ahough@masterbuilt.com>
2021-09-19 00:09:23 -0600

    push up w/o secrets

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,12 @@
 import nicehash
 import requests
 
+host = 'https://api2.nicehash.com'
+organisation_id = 'Enter your organisation id'
+key = 'Enter your api key'
+secret = 'Enter your secret for api key'
+plug1_off_wh = 'IFTT webhook url'
+plug1_on_wh = 'IFTT webhook url'
 
 private_api = nicehash.private_api(host, organisation_id, key, secret)
 
